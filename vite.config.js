cfeb44c841675318f64e8a51ef851e795c5660b4
Santiago <santisevilla.9@gmail.com>
2024-09-05 18:46:31 -0300

    asdsad

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://santisevillaa.github.io/santix-Reto05",
+  base: "https://santisevillaa.github.io/Reto05",
 })
